44e6ff0ae65555bef11c24230611e68953931a3e
louis bemberg <louis.bemberg@gmail.com>
2020-06-22 20:50:50 +0200

    Map feature, markers, and address autocomplete are DONE

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,16 +1,24 @@
 class Event < ApplicationRecord
+  # 1-N relationship: a post belongs to a host
   belongs_to :user
-  has_many :users, through: :reviews
 
+  # N-N Relationship between Event and User, connected by join table >messages<
   has_many :messages
   has_many :users, through: :messages
 
+  # N-N Relationship between Event and User, connected by join table >favourites<
   has_many :favourites
   has_many :users, through: :favourites
 
+  # N-N Relationship between Event and User, connected by join table >event_users<
   has_many :event_users
   has_many :users, through: :event_users
 
+  # N-N Relationship between Event and User, connected by join table >reviews<
   has_many :reviews
   has_many :users, through: :reviews
+
+  # Necessary for Maps and Geocoding
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,21 +1,28 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # 1-N Relationship: a user has many bucket list items (called goals for simplicity)
   has_many :goals
+
+  # 1-N Relationship: a host has many events
   has_many :events
 
+  # N-N Relationship between Event and User, connected by join table >reviews<
   has_many :reviews
   has_many :events, through: :reviews
 
+  # N-N Relationship between Event and User, connected by join table >messages<
   has_many :messages
   has_many :events, through: :messages
 
+  # N-N Relationship between Event and User, connected by join table >favourites<
   has_many :favourites
   has_many :events, through: :favourites
 
+  # N-N Relationship between Event and User, connected by >event_users<
   has_many :event_users
   has_many :events, through: :event_users
 
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
