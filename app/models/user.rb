3412b460670df91d6fc408f18b695ff196623b38
David Hernandez Azagury <davidhazagury@gmail.com>
2020-06-23 12:56:23 +0200

    Log Out button added

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ApplicationRecord
-  #validates :first_name, presence: true
-  #validates :last_name, presence: true
+
   # 1-N Relationship: a user has many bucket list items (called goals for simplicity)
 
   # Include default devise modules. Others available are:
