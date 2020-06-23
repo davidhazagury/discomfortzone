8a5ad7683aee2be26ae540c3b8bd4b36b5ef5c8a
Wolfgang Alkier <w.alkier@gmx.at>
2020-06-23 14:39:32 +0200

    index n shit

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,18 +1,7 @@
 class EventsController < ApplicationController
   def index
     @events = Event.all
-    # -----------------------------------------------------------
-    # Ignore the code below, this is only in case we need a map on the index page:)
-    @events_geocoded = Event.geocoded # <= returns all the events that have been saved with an address
-    @markers = @flats.map do |event| # <= creates an array of all the markers from ^
-      {
-        lat: event.latitude,
-        lng: event.longitude,
-        infoWindow: render_to_string(partial: "info_window", locals: { event: event })
-      }
-    end
-    # -----------------------------------------------------------
-  end
+   end
 
   def show
     @event = Event.find(params[:id])
