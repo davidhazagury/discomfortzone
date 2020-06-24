78799689abe37dd353b9c9bfa56f26dbd6a813c1
David Hernandez Azagury <davidhazagury@gmail.com>
2020-06-24 17:34:22 +0200

    Nice calendar displaying for picking up dates

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -29,11 +29,12 @@ import "bootstrap";
 // import { initSelect2 } from '../components/init_select2';
 import { initMapbox } from '../plugins/init_mapbox';
 import { initAutocomplete } from '../plugins/init_autocomplete';
-
+import { initFlatpickr } from '../plugins/init_flatpickr'
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
+  initFlatpickr();
   initMapbox();
   initAutocomplete();
 });
