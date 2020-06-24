b4b43873ed392439bf18468b9ead8a81b8231d40
jonas fürstenberg <jonas.fuerstenberg99@gmail.com>
2020-06-25 00:36:50 +0800

    Updated the Seeds with pictures, plus the index card CSS

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,18 +12,42 @@ EventUser.destroy_all
 Event.destroy_all
 User.destroy_all
 
-puts "2 - Let's create some users..."
-user1 = User.create!(email: "musk@gmail.com", password: "1234567", first_name: "elon", last_name:"musk")
+puts "2 - Let's create some users ... with pictures"
 user2 = User.create!(email: "elon@gmail.com", password: "1234567", first_name: "CEO", last_name:"SPACEX")
 user3 = User.create!(email: "louis@gmail.com", password: "1234567", first_name: "Louis", last_name:"Bemberg")
 user4 = User.create!(email: "david@gmail.com", password: "1234567", first_name: "David", last_name:"Hernandez")
 user5 = User.create!(email: "wolfgang@gmail.com", password: "1234567", first_name: "Wolfgang", last_name:"Alkier")
-user6 = User.create!(email: "jonas@gmail.com", password: "1234567", first_name: "Jonas", last_name:"Fuerstenberg")
-user7 = User.create(email: "steve@gmail.com", password: "1234567", first_name: "steve",last_name: 'jobs')
-user8 = User.create(email: "vader@gmail.com", password: "1234567", first_name: "darth",last_name: 'vader')
 
+
+elon_pic = URI.open ("https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cg_face%2Cq_auto:good%2Cw_300/MTY2MzU3Nzk2OTM2MjMwNTkx/elon_musk_royal_society.jpg")
+user1 = User.new(email: "musk@gmail.com", password: "1234567", first_name: "elon", last_name:"musk")
+user1.photo.attach(io: elon_pic, filename: 'elon.png', content_type: 'image/png')
+user1.save!
+
+
+vader_pic = URI.open ("https://avatars.alphacoders.com/avatars/view/177869")
+user8 = User.new(email: "vader@gmail.com", password: "1234567", first_name: "darth",last_name: 'vader')
+user8.photo.attach(io: vader_pic, filename: 'vader.png', content_type: 'image/png')
+user8.save!
+
+steve_pic = URI.open ("https://www.pioneeringminds.com/wp-content/uploads/2018/10/Steve-Jobs-1440x960.jpg")
+user7 = User.new(email: "steve@gmail.com", password: "1234567", first_name: "steve",last_name: 'jobs')
+user7.photo.attach(io: steve_pic, filename: 'steve.png', content_type: 'image/png')
+user7.save!
+
+jonas_pic = URI.open ("http://res.cloudinary.com/dgsqurpio/image/upload/v1593009378/jauae2tbjyzjuhccirbg.jpg")
+user6 = User.new(email: "jonas@gmail.com", password: "1234567", first_name: "Jonas", last_name:"Fuerstenberg")
+user6.photo.attach(io: jonas_pic, filename: 'jonas.png', content_type: 'jonas/png')
+user6.save!
 
 puts "3 - Let's create some events..."
+
+# HIKING TRIP THAT LASTS 2 DAYS WITH A LONG DESCRIPTION
+rainbow_mountain = URI.open("https://res.cloudinary.com/dgsqurpio/image/upload/v1592991124/nqp81ovayvop227ne42g.jpg")
+hiking = Event.new(title: "special-hike", description: "Early birds catches the worm and the best light in the world !! We are gonna enjoy the most beautiful landscape that vienna has to offer. Please be advised to bring proper equipment since the trip will be the whole weekend - that includes a Hiking backpack
+Weather-appropriate clothing (think moisture-wicking and layers), Hiking boots or shoes, Plenty of food, Plenty of water, sleeping bag, some toold like a knife.. As the host I will bring a First-aid kit as I'm a trained medic. THIS IS A REAL OUTDOOR ADVENTURE, whoooo !!", address: "vienna", capacity: 5, user_id: user3.id, start_time: "2020-08-10 12:00", end_time: "2020-08-12 18:00")
+hiking.photo.attach(io: rainbow_mountain, filename: 'rainbow_mountain.png', content_type: 'image/png')
+hiking.save!
 
 # Shark Cage Diving
 skydiving_picture = URI.open('https://source.unsplash.com/1600x900/?adventure')
@@ -43,9 +67,13 @@ randomcity = Event.new(title: "Travel to a Random City", description: "Let's jum
 randomcity.photo.attach(io: randomcity_picture, filename: 'randomcity.png', content_type: 'image/png')
 randomcity.save!
 
-# more events, without pictures tho
-event2 = Event.create(title: "paragliding", description: "Exhileration experience just floating through the air and through the clouds", address: "switzerland", capacity: 4, user_id: user5.id)
-event3 = Event.create(title: "special-hike", description: "Early birds catches the worm and the best light in the world ", address: "vienna", capacity: 5, user_id: user3.id)
+
+# PARAGLIDING
+paragliding_pic = URI.open("https://res.cloudinary.com/dgsqurpio/image/upload/v1592991090/v4fg14hfhizujxdvucjw.jpg")
+paragliding = Event.new(title: "paragliding", description: "Exhileration experience just floating through the air and through the clouds", address: "switzerland", capacity: 4, user_id: user5.id, start_time: "2020-07-12 12:00", end_time: "2020-07-12 18:00")
+paragliding.photo.attach(io: paragliding_pic, filename: 'paragliding.png', content_type: 'image/png')
+paragliding.save!
+
 
 puts "4 - Let's match some users with some events (event_user)"
 # Jonas, David and Wolfgang applying to Lou's bungee jumping
