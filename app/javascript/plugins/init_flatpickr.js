9bf85b0439070a607850c1669149e2eeb4903c23
Wolfgang Alkier <w.alkier@gmx.at>
2020-06-26 16:09:38 +0200

    changes with the boys

--- app/javascript/plugins/init_flatpickr.js
+++ app/javascript/plugins/init_flatpickr.js
@@ -1,18 +1,18 @@
-// import flatpickr from "flatpickr";
-// const initFlatpickr = () => {
-//   const elements = document.querySelectorAll(".calendar")
-//   elements.forEach((element) => {
-//     flatpickr(element, {
-//       enableTime: true,
-//       dateFormat: "Y-m-d H:i",
-//     });
-//   })
+import flatpickr from "flatpickr";
+const initFlatpickr = () => {
+  const elements = document.querySelectorAll(".calendar")
+  elements.forEach((element) => {
+    flatpickr(element, {
+      enableTime: true,
+      dateFormat: "Y-m-d H:i",
+    });
+  })
 
 
-// }
+}
 
 
 
 
 
-// export { initFlatpickr }
+export { initFlatpickr }
